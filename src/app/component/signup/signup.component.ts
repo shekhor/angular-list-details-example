80c9b9c7162adf49cd6551c2d73afd11a3e41392
Shekhor Chanda <shekhor.chanda333@gmail.com>
2025-01-29 20:03:03 -0600

    Added custom validation.

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -15,9 +15,21 @@ export class SignupComponent {
       name: new FormControl('', [Validators.required, Validators.minLength(5)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       phone: new FormControl(''),
-      password: new FormControl('')
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)])
+    },
+    {validators: this.confirmPasswordValidator}
+    );
+  }
+
+  confirmPasswordValidator(group: AbstractControl) {
+    let password1 = group.get('password')?.value;
+    let password2 = group.get('confirmPassword')?.value;
+
+    if(password1 && password2 && password1 !== password2){
+      return {passwordMismatch: true};
     }
-    )
+    return null;
   }
   onSubmit(){
     console.log("Name is:", this.myForm.value.name);
